33294c28629bb4b363f1e7f63e2de5123a03e35c
Aytaditya <adityaaryan531@gmail.com>
2024-07-17 05:38:56 +0530

    kinde auth implemented using gmail

--- src/components/home/auth-screen/AuthButtons.tsx
+++ src/components/home/auth-screen/AuthButtons.tsx
@@ -1,11 +1,20 @@
+"use client"
 import { Button } from "@/components/ui/button"
+import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs"
+import { useState } from "react"
 
 const AuthButtons = () => {
+  const [loading,setLoading]=useState(false)
   return (
     <div className="flex gap-3 flex-1 md:flex-row flex-col">
-
-      <Button className="w-full" variant={"outline"}>Sign Up</Button>
-      <Button className="w-full">Log In</Button>
+      <RegisterLink className="flex-1">
+      <Button className="w-full" variant={"outline"} 
+      onClick={()=>setLoading(true)} disabled={loading}>Sign Up</Button>
+      </RegisterLink>
+      <LoginLink className="flex-1">
+      <Button className="w-full"
+      onClick={()=>setLoading(true)} disabled={loading}>Log In</Button>
+      </LoginLink>
       
     </div>
   )
